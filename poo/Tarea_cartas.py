2f300aa6dbac96ef00ae6660fb182dfd96171947
HiramTorres <hiram.torres16@my.unitec.edu.mx>
2022-03-24 22:59:27 -0600

    Correccion en lineas de Tarea_cartas.py

--- poo/Tarea_cartas.py
+++ poo/Tarea_cartas.py
@@ -1,33 +1,48 @@
 from random import randint
 
+
 class Carta:
+
     palo = ""
+
     valor = ""
 
+
     def __init__(self,palo,valor):
+
         self.palo = palo
+
         self.valor = valor
 
     def __str__(self):
+
         return self.valor + " de " + self.palo
 
     
     
 
+
 class baraja:
     def __init__(self,cartas):
 
+
         self.cartas = cartas
     
-    def robar_carta(self,carta):
-        carta = deck[randint(0,len(deck)-1)]
-        deck.remove(carta)
-        return carta
+
+    def robar_carta(self,cartas):
+        deck.remove(deck[randint(0,len(deck)-1)])
+        return deck[randint(0,len(deck)-1)]
+
 
     def shuffle(self):
+
         for i in range(len(self.cartas)-1,0,-1):
+
             j = randint(0,i)
+
             self.cartas[i],self.cartas[j] = self.cartas[j],self.cartas[i]
+
         return self.cartas
+
 
 
